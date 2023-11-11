503ecc1ec3723fca7f4a1836dc15eada9766d952
Chathura Colombage <dcdewaka@gmail.com>
2023-11-11 21:54:32 +0000

    cleaner version

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -2,22 +2,22 @@ package erratum
 
 func Use(opener ResourceOpener, input string) (err error) {
 	var res Resource
-	for {
-		res, err = opener()
-		if err == nil {
-			break
-		}
+	for res, err = opener(); err != nil; res, err = opener() {
 		if _, ok := err.(TransientError); !ok {
 			return err
 		}
 	}
 	defer res.Close()
+
 	defer func() {
 		if r := recover(); r != nil {
-			if frobErr, ok := r.(FrobError); ok {
-				res.Defrob(frobErr.defrobTag)
+			switch e := r.(type) {
+			case FrobError:
+				res.Defrob(e.defrobTag)
+				err = e.inner
+			case error:
+				err = e
 			}
-			err = r.(error)
 		}
 	}()
 	res.Frob(input)
